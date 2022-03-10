e94caf26c56b4c02467b6e1b37ae4d76a9c291c7
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-03-10 09:18:15 -0800

    Add Adapter and RecyclerView

---
+++ app/src/main/java/com/chaveze/affirmationsapp/adapter/ItemAdapter.kt
@@ -0,0 +1,37 @@
+package com.chaveze.affirmationsapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.chaveze.affirmationsapp.R
+import com.chaveze.affirmationsapp.model.Affirmation
+
+class ItemAdapter(
+    val ctx: Context,
+    private val dataset: List<Affirmation>)
+        : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val v: View): RecyclerView.ViewHolder(v) {
+        val tv: TextView = v.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+
+        holder.tv.text = ctx.resources.getString(item.stringResId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
